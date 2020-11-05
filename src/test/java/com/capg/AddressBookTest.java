159bf3114fbf272af129567d13b7056d22bc55f4
mehakjit <mehakjitsingh@gmail.com>
2020-11-05 14:38:01 +0530

    [Mehakjit] Added: Test method to check all the individual blocks

--- src/test/java/com/capg/AddressBookTest.java
+++ src/test/java/com/capg/AddressBookTest.java
@@ -1,5 +1,17 @@
 package com.capg;
 
-public class AddressBookTest {
+import java.util.List;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.capg.AddressBookService.IOService;
+
+public class AddressBookTest {
+	@Test
+	    public void givenEmpPayrollDataInDB_ShouldMatchEmpCount() {
+	    	AddressBookService addBookService = new AddressBookService();
+	    	List<AddressBookData> addBookData = addBookService.readAddresBookData(IOService.DB_IO);
+	    	Assert.assertEquals(6, addBookData.size());
+	    }
 }
